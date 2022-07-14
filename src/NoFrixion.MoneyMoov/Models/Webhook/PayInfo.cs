860a096c3ca6e94305af19842a9ea61b67f4262e
Pablo Maldonado <pablomartinmaldonado1@gmail.com>
2022-07-14 13:27:08 +0100

    Add Webhook DTOs

---
+++ src/NoFrixion.MoneyMoov/Models/Webhook/PayInfo.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+#nullable disable
+
+namespace NoFrixion.MoneyMoov.Models
+{
+    public class PayInfo
+    {
+        // Only available for PAYOUT
+        [JsonProperty("id", NullValueHandling = NullValueHandling.Ignore)]
+        public Guid? Id { get; set; }
+
+        [JsonProperty("transactionId")]
+        public string TransactionId { get; set; }
+
+        [JsonProperty("amount")]
+        public decimal Amount { get; set; }
+
+        [JsonProperty("currency")]
+        public string Currency { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("date")]
+        public DateTimeOffset Date { get; set; }
+
+        [JsonProperty("from")]
+        public AccountInfo From { get; set; }
+
+        [JsonProperty("to")]
+        public AccountInfo To { get; set; }
+    }
+}
