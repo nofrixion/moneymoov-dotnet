4345d015ee85f407b7e2e384419de66ff63a42b5
Saurav Maiti <saurav@nofrixion.com>
2022-07-19 09:58:20 +0100

    Moved UserRolesEnum to moneymoov SDK and created UserRole class.

---
+++ src/NoFrixion.MoneyMoov/Models/User/UserRole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NoFrixion.MoneyMoov.Enums;
+
+namespace NoFrixion.MoneyMoov.Models
+{
+    public class UserRole
+    {
+        public Guid UserID { get; set; }
+
+        public Guid MerchantID { get; set; }
+
+        public UserRolesEnum RoleType { get; set; }
+    }
+}
