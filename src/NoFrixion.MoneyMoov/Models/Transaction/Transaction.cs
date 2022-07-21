48256c33342b137f1dd5f57a54a05c8eb9768ead
Pablo Maldonado <pablomartinmaldonado1@gmail.com>
2022-07-21 09:37:54 +0100

    Update Transaction DTO
    and add page response DTO

---
+++ src/NoFrixion.MoneyMoov/Models/Transaction/Transaction.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json;
+using System.Runtime.Serialization;
+
+#nullable disable
+
+namespace NoFrixion.MoneyMoov.Models
+{
+    public class Transaction
+    {
+        /// <summary>
+        /// Transaction id which always starts with 'T'.
+        /// </summary>
+        [JsonProperty("transactionId")]
+        public string TransactionId { get; set; }
+
+        /// <summary>
+        /// Id of the Payout
+        /// <para>Only available for transaction type <see cref="TransactionType.PAYOUT"/></para>
+        /// </summary>
+        [JsonProperty("id", NullValueHandling = NullValueHandling.Ignore)]
+        public Guid? Id { get; set; }
+
+        /// <summary>
+        /// Type of the transaction
+        /// <para>Available options are <see cref="TransactionType.PAYOUT"/> and <see cref="TransactionType.PAYIN"/></para>
+        /// </summary>
+        [JsonProperty("type")]
+        public TransactionType Type { get; set; }
+
+        /// <summary>
+        /// Amount transfered
+        /// </summary>
+        [JsonProperty("amount")]
+        public decimal Amount { get; set; }
+
+        /// <summary>
+        /// Currency of amount
+        /// </summary>
+        [JsonProperty("currency")]
+        public string Currency { get; set; }
+
+        /// <summary>
+        /// Description of the transaction
+        /// </summary>
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Date when the transaction was created (in UTC)
+        /// </summary>
+        [JsonProperty("createdAt")]
+        public DateTimeOffset CreatedAt { get; set; }
+
+        /// <summary>
+        /// Date when the transaction happened (in UTC)
+        /// </summary>
+        [JsonProperty("date")]
+        public DateTimeOffset Date { get; set; }
+
+        /// <summary>
+        /// Account of who sent the transaction
+        /// </summary>
+        [JsonProperty("from")]
+        public AccountInfo From { get; set; }
+
+        /// <summary>
+        /// Account of who receive the transaction
+        /// </summary>
+        [JsonProperty("to")]
+        public AccountInfo To { get; set; }
+    }
+
+    public enum TransactionType
+    {
+        [EnumMember(Value = "NONE")]
+        NONE,
+
+        [EnumMember(Value = "PAY_IN")]
+        PAYIN,
+
+        [EnumMember(Value = "PAY_OUT")]
+        PAYOUT
+    }
+}
---
+++ src/NoFrixion.MoneyMoov/Models/Utils/PageResponse.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+
+#nullable disable
+
+namespace NoFrixion.MoneyMoov.Models.Utils
+{
+    public class PageResponse<T>
+    {
+        [JsonProperty("content")]
+        public T Content { get; set; }
+
+        /// <summary>
+        /// Current page number. Its 0 based. i.e firstpage &#x3D;0, secondpage&#x3D;1 
+        /// </summary>
+        /// <value>Current page number. Its 0 based. i.e firstpage &#x3D;0, secondpage&#x3D;1 </value>
+        [JsonProperty("page")]
+        public int Page { get; set; }
+
+        /// <summary>
+        /// Page size
+        /// </summary>
+        /// <value>Page size</value>
+        [JsonProperty("size")]
+        public int Size { get; set; }
+
+        /// <summary>
+        /// Total pages
+        /// </summary>
+        /// <value>Total pages</value>
+        [JsonProperty("totalPages")]
+        public int TotalPages { get; set; }
+
+        /// <summary>
+        /// Total count
+        /// </summary>
+        /// <value>Total count</value>
+        [JsonProperty("totalSize")]
+        public long TotalSize { get; set; }
+
+        public override string ToString()
+        {
+            return $"{nameof(Content)}: {Content}, {nameof(Page)}: {Page}, {nameof(Size)}: {Size}, {nameof(TotalPages)}: {TotalPages}, {nameof(TotalSize)}: {TotalSize}";
+        }
+    }
+}
