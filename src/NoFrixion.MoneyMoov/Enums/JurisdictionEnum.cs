9bf62309652d550f0e45b571f73919e7175c5707
Aaron Clauson <aaron@nofrixion.com>
2022-12-14 23:19:44 +0000

    Added jurisdiction to merchant.

---
+++ src/NoFrixion.MoneyMoov/Enums/JurisdictionEnum.cs
@@ -0,0 +1,35 @@
+﻿//-----------------------------------------------------------------------------
+// Filename: JurisdictionEnum.cs
+// 
+// Description: List of the jurisdictions, for companies and merchants,
+// supported by NoFrixion.
+//
+// Author(s):
+// Aaron Clauson (aaron@nofrixion.com)
+// 
+// History:
+// 14 Dec 2021  Aaron Clauson   Migrated from compliance project.
+// 
+// License:
+// MIT.
+//-----------------------------------------------------------------------------
+
+namespace NoFrixion.MoneyMoov;
+
+public enum JurisdictionEnum
+{
+    /// <summary>
+    /// Ireland.
+    /// </summary>
+    IE,
+
+    /// <summary>
+    /// United Kingdom.
+    /// </summary>
+    UK,
+
+    /// <summary>
+    /// European Union, other than Ireland.
+    /// </summary>
+    EU
+}
