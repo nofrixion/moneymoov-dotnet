7c693a5a809fc691bdbae61ed6f45ddce920a117
Aaron Clauson <aaron@nofrixion.com>
2023-01-07 18:50:16 +0000

    Updated transaction and account info models.

--- src/NoFrixion.MoneyMoov/Models/Transaction/Transaction.cs
+++ src/NoFrixion.MoneyMoov/Models/Transaction/Transaction.cs
@@ -13,6 +13,11 @@ namespace NoFrixion.MoneyMoov.Models
         public Guid ID { get; set; }
 
         /// <summary>
+        /// The ID of the account the transaction belongs to.
+        /// </summary>
+        public Guid AccountID { get; set; }
+
+        /// <summary>
         /// Type of the transaction.
         /// </summary>
         public TransactionTypesEnum Type { get; set; }
@@ -49,15 +54,32 @@ namespace NoFrixion.MoneyMoov.Models
         public decimal Balance { get; set; }
 
         /// <summary>
+        /// For a pay in the reference the sending party attached. For a pay out the 
+        /// reference that the payer attached for themselves.
+        /// </summary>
+        public string YourReference { get; set; }
+
+        /// <summary>
+        /// For a pay out the reference that the payer attached for the receiving party. For a
+        /// pay in this will typically be empty but for internal transactions may contain the
+        /// reference the sending party set for themselves.
+        /// </summary>
+        public string TheirReference { get; set; }
+
+        /// <summary>
         /// Account of who sent the transaction
         /// </summary>
         [JsonProperty("from")]
         public AccountInfo From { get; set; }
+
+        public string FromDisplayText { get; set; }
 
         /// <summary>
         /// Account of who receive the transaction
         /// </summary>
         [JsonProperty("to")]
         public AccountInfo To { get; set; }
+
+        public string ToDisplayText { get; set; }
     }
 }
