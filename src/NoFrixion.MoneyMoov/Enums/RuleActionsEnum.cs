0799961555b68df043cef84217aecdf6484dc973
Aaron Clauson <aaron@nofrixion.com>
2023-01-23 09:56:04 +0000

    Move rule biz models from api project. (#43)

---
+++ src/NoFrixion.MoneyMoov/Enums/RuleActionsEnum.cs
@@ -0,0 +1,36 @@
+﻿//-----------------------------------------------------------------------------
+// Filename: RulesActionsEnum.cs
+// 
+// Description: A list of the supported Rule Actions, or executable steps, for
+// a MoneyMoov Rule.
+// 
+// Author(s):
+// Aaron Clauson (aaron@nofrixion.com)
+// 
+// History:
+// 22 Jan 2023  Aaron Clauson   Created, Harcourt Street, Dublin, Ireland.
+// 
+// License:
+// MIT.
+//-----------------------------------------------------------------------------
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace NoFrixion.MoneyMoov;
+
+[JsonConverter(typeof(StringEnumConverter))]
+public enum RuleActionsEnum
+{
+    IsAlive,
+
+    Greeter,
+
+    Payout,
+
+    PayoutPercentage,
+
+    Split,
+
+    Sweep
+}
