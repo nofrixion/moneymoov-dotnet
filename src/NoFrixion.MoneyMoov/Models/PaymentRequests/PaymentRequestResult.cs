a7fe0ad22e63a9d9673c4a0f1307fb7eeff41936
Saurav Maiti <saurav@nofrixion.com>
2023-02-22 18:04:22 +0000

    Fixed issue with wrong outstanding amount calculation. It was hapenning because of payments of 1 EURO.

--- src/NoFrixion.MoneyMoov/Models/PaymentRequests/PaymentRequestResult.cs
+++ src/NoFrixion.MoneyMoov/Models/PaymentRequests/PaymentRequestResult.cs
@@ -133,11 +133,11 @@ public class PaymentRequestResult
                           payEvent.Status == PISP_YAPILY_PENDING_STATUS))
                 {
                     // Successfully authorised payment initiation.
-                    if (!string.IsNullOrEmpty(payEvent.PispPaymentInitiationID) && 
+                    if (!string.IsNullOrEmpty(payEvent.PispPaymentInitiationID) &&
                         !PispAuthorizations.Any(x => x.PispPaymentInitiationID == payEvent.PispPaymentInitiationID) &&
                         (
-                            (payEvent.Currency == CurrencyTypeEnum.EUR && payEvent.Amount > PaymentsConstants.PISP_MINIMUM_EUR_PAYMENT_AMOUNT) ||
-                            (payEvent.Currency == CurrencyTypeEnum.GBP && payEvent.Amount > PaymentsConstants.PISP_MINIMUM_GBP_PAYMENT_AMOUNT)
+                            (payEvent.Currency == CurrencyTypeEnum.EUR && payEvent.Amount >= PaymentsConstants.PISP_MINIMUM_EUR_PAYMENT_AMOUNT) ||
+                            (payEvent.Currency == CurrencyTypeEnum.GBP && payEvent.Amount >= PaymentsConstants.PISP_MINIMUM_GBP_PAYMENT_AMOUNT)
                         ) 
                         ) 
                     {
