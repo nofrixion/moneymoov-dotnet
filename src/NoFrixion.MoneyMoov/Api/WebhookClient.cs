47a3588b1999ecdec60fc132161ec77dae3132e5
Aaron Clauson <aaron@nofrixion.com>
2023-03-12 21:55:11 +0000

    Added webhook models.

---
+++ src/NoFrixion.MoneyMoov/Api/WebhookClient.cs
@@ -0,0 +1,66 @@
+﻿//-----------------------------------------------------------------------------
+// Filename: WebHooksClient.cs
+//
+// Description: An API client to call the MoneyMoov WebHooks API end point.
+//
+// Author(s):
+// Aaron Clauson (aaron@nofrixion.com)
+// 
+// History:
+// 12 Mar 2023  Aaron Clauson   Created, Stillorgan Wood, Dublin, Ireland.
+//
+// License: 
+// MIT.
+//-----------------------------------------------------------------------------
+
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using NoFrixion.MoneyMoov.Models;
+
+namespace NoFrixion.MoneyMoov;
+
+public interface IWebhookClient
+{
+    Task<MoneyMoovApiResponse<Webhook>> CreateWebhookAsync(WebhookCreate webhookCreate);
+}
+
+public class WebhookClient : IWebhookClient
+{
+    private readonly ILogger _logger;
+    private readonly IMoneyMoovApiClient _apiClient;
+
+    public WebhookClient()
+    {
+        _apiClient = new MoneyMoovApiClient(MoneyMoovUrlBuilder.DEFAULT_MONEYMOOV_BASE_URL);
+        _logger = NullLogger.Instance;
+    }
+
+    public WebhookClient(string baseUri)
+    {
+        _apiClient = new MoneyMoovApiClient(baseUri);
+        _logger = NullLogger.Instance;
+    }
+
+    public WebhookClient(IMoneyMoovApiClient apiClient)
+    {
+        _apiClient = apiClient;
+        _logger = NullLogger.Instance;
+    }
+
+    public WebhookClient(IMoneyMoovApiClient apiClient, ILogger<AccountClient> logger)
+    {
+        _apiClient = apiClient;
+        _logger = logger;
+    }
+
+    /// <summary>
+    /// Calls the MoneyMoov Merchant get user endpoint create a new webhook record.
+    /// </summary>
+    /// <param name="webHookCreate">The model containing the data about the new webhook to create.</param>
+    /// <returns>If successful, a webhook object.</returns>
+    public Task<MoneyMoovApiResponse<Webhook>> CreateWebhookAsync(WebhookCreate webHookCreate)
+    {
+        var url = MoneyMoovUrlBuilder.WebhooksApi.WebhooksUrl(_apiClient.GetBaseUri().ToString());
+        return _apiClient.PostAsync<Webhook>(url, new FormUrlEncodedContent(webHookCreate.ToDictionary()));
+    } 
+}
--- src/NoFrixion.MoneyMoov/MoneyMoovUrlBuilder.cs
+++ src/NoFrixion.MoneyMoov/MoneyMoovUrlBuilder.cs
@@ -85,6 +85,9 @@ public static class MoneyMoovUrlBuilder
         public static string MerchantAccountsUrl(string moneyMoovBaseUrl, Guid merchantID)
             => $"{moneyMoovBaseUrl}/{MoneyMoovResources.merchants}/{merchantID}/{MoneyMoovResources.accounts}";
 
+        public static string MerchantBankSettings(string moneyMoovBaseUrl, Guid merchantID)
+            => $"{moneyMoovBaseUrl}/{MoneyMoovResources.merchants}/{merchantID}/{MoneyMoovResources.banksettings}";
+
         public static string MerchantTokensUrl(string moneyMoovBaseUrl, Guid merchantID)
             => $"{moneyMoovBaseUrl}/{MoneyMoovResources.merchants}/{merchantID}/{MoneyMoovResources.tokens}";
 
@@ -97,8 +100,8 @@ public static class MoneyMoovUrlBuilder
         public static string MerchantUserRoleUrl(string moneyMoovBaseUrl)
             => $"{moneyMoovBaseUrl}/{MoneyMoovResources.merchants}/{MoneyMoovResources.userroles}";
         
-        public static string MerchantBankSettings(string moneyMoovBaseUrl, Guid merchantID)
-            => $"{moneyMoovBaseUrl}/{MoneyMoovResources.merchants}/{merchantID}/{MoneyMoovResources.banksettings}";
+        public static string MerchantWebHooksUrl(string moneyMoovBaseUrl, Guid merchantID)
+            => $"{moneyMoovBaseUrl}/{MoneyMoovResources.merchants}/{merchantID}/{MoneyMoovResources.webhooks}";
     }
 
     /// <summary>
@@ -216,6 +219,21 @@ public static class MoneyMoovUrlBuilder
 
         public static string UserInviteUrl(string moneyMoovBaseUrl, Guid userInviteID)
             => $"{moneyMoovBaseUrl}/{MoneyMoovResources.userinvites}/{userInviteID}";
+    }
+
+    /// <summary>
+    /// Available endpoint URLs for the merchant webhooks resource.
+    /// </summary>
+    public static class WebhooksApi
+    {
+        public static string WebhookUrl(string moneyMoovBaseUrl, Guid id)
+            => $"{moneyMoovBaseUrl}/{MoneyMoovResources.webhooks}/{id}";
+
+        public static string WebhooksUrl(string moneyMoovBaseUrl)
+            => $"{moneyMoovBaseUrl}/{MoneyMoovResources.webhooks}";
+
+        public static string AllWebhooksUrl(string moneyMoovBaseUrl, Guid merchantID)
+            => $"{moneyMoovBaseUrl}/{MoneyMoovResources.webhooks}/{merchantID}";
     }
 
     public static string AccountStatementApiUrl(string moneyMoovBaseUrl, string? accountId = null)
