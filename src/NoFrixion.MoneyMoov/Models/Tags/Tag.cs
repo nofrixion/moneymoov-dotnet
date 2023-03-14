56cb7ab6211144fce170e4846176a927c0c64434
Aaron Clauson <aaron@nofrixion.com>
2023-03-14 11:44:03 +0000

    Added tag model.

---
+++ src/NoFrixion.MoneyMoov/Models/Tags/Tag.cs
@@ -0,0 +1,27 @@
+﻿//-----------------------------------------------------------------------------
+// Filename: Tag.cs
+//
+// Description: Represents a structure for attaching a descriptive tag to an
+// entity, such as a payment request.
+//
+// Author(s):
+// Aaron Clauson (aaron@nofrixion.com)
+// 
+// History:
+// 14 Mar 2023  Aaron Clauson       Created, Harcourt St, Dublin, Ireland.
+//
+// License: 
+// Proprietary NoFrixion.
+//-----------------------------------------------------------------------------
+
+namespace NoFrixion.MoneyMoov.Models;
+
+public class Tag
+{
+    public Guid ID { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string ColourHex { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public DateTimeOffset Inserted { get; set; }
+    public DateTimeOffset LastUpdated { get; set; }
+}
