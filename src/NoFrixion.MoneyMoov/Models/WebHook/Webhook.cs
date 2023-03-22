3b58b1b44a99cc18c347d295ec5e350c2a39f998
Aaron Clauson <aaron@nofrixion.com>
2023-03-22 07:51:11 +0000

    Client webhook (#97)
    * wip: wire up client to create webhooks.
    
    * Add webhook create max secret length validation.
    
    * wip: add nfxn strong sig.
    
    * Adjusted webhook signature generation method.
    
    * Added new paylaod signature to webhook.

--- src/NoFrixion.MoneyMoov/Models/WebHook/Webhook.cs
+++ src/NoFrixion.MoneyMoov/Models/WebHook/Webhook.cs
@@ -12,10 +12,19 @@
 //  MIT.
 // -----------------------------------------------------------------------------
 
+using System.Security.Cryptography;
+using System.Text;
+
 namespace NoFrixion.MoneyMoov.Models;
 
 public class Webhook
 {
+    /// <summary>
+    /// The name of the HTTP header that the MoneyMoov API server sets for teh webhook's
+    /// payload signature.
+    /// </summary>
+    public const string MONEYMOOV_SIGNATURE_HEADER = "x-moneymoov-signature";
+
     public Guid ID { get; set; }
 
     public WebhookEventTypesEum Type { get; set; }
@@ -29,4 +38,16 @@ public class Webhook
     public bool IsActive { get; set; } = true;
 
     public string? EmailAddress { get; set; }
+
+    public static string GetSignature(string secret, byte[] payloadBytes)
+    {
+        byte[] keyByte = ASCIIEncoding.UTF8.GetBytes(secret);
+
+        using (var hmac = new HMACSHA256(keyByte))
+        {
+            byte[] hashMessage = hmac.ComputeHash(payloadBytes);
+            var hashMsg = Convert.ToBase64String(hashMessage);
+            return Convert.ToBase64String(hashMessage);
+        }
+    }
 }
