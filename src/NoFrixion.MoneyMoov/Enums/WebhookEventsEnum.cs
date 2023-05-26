740290b58c7eb7af68f2d1462808361f0d7b1b7c
Aaron Clauson <aaron@nofrixion.com>
2023-05-26 16:54:12 +0100

    Add new webhook models.

---
+++ src/NoFrixion.MoneyMoov/Enums/WebhookEventsEnum.cs
@@ -0,0 +1,25 @@
+﻿//-----------------------------------------------------------------------------
+// Filename: WebhookEventTypesEnum.cs
+// 
+// Description: Enum for the different webhook event types.
+// 
+// Author(s):
+// Aaron Clauson (aaron@nofrixion.com)
+// 
+// History:
+// 26 May 2023 Aaron Clauson   Created, Harcourt St, Dublin, Ireland.
+// 
+// License:
+// MIT.
+//-----------------------------------------------------------------------------
+
+namespace NoFrixion.MoneyMoov;
+
+public enum WebhookEventsEnum
+{
+    Created,
+
+    Updated,
+
+    Deteted
+}
