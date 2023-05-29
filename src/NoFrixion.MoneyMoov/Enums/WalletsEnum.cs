0380716cc960a0c726df96f0cce4b1b1e521d630
sauravmaiti22 <103389834+sauravmaiti22@users.noreply.github.com>
2023-05-29 14:59:56 +0100

    Added wallet name to payment attempt.

---
+++ src/NoFrixion.MoneyMoov/Enums/WalletsEnum.cs
@@ -0,0 +1,23 @@
+﻿// -----------------------------------------------------------------------------
+//  Filename: WalletsEnum.cs
+// 
+//  Description: Enum for wallets
+// 
+//  Author(s):
+//  Saurav Maiti (saurav@nofrixion.com)
+// 
+//  History:
+//  29 05 2023  Saurav Maiti Created, Harcourt Street, Dublin, Ireland.
+// 
+//  License:
+//  Proprietary NoFrixion.
+// -----------------------------------------------------------------------------
+
+namespace NoFrixion.MoneyMoov;
+
+public enum WalletsEnum
+{
+    ApplePay,
+    
+    GooglePay
+}
