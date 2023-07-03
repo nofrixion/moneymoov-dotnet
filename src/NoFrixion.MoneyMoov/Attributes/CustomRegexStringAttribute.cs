2d1ed7d20f21da7c8f450757cfd1bfedf2959b4a
Arif Matin <arif@nofrixion.com>
2023-07-03 16:26:34 +0100

    Added validation for create merchant business associate names

---
+++ src/NoFrixion.MoneyMoov/Attributes/CustomRegexStringAttribute.cs
@@ -0,0 +1,42 @@
+﻿//  Filename: CustomRegexStringAttribute.cs
+// 
+//  Description: A custom attribute to validate string value with a regular expression:
+// 
+//  Author(s):
+//  Arif Matin (arif@nofrixion.com)
+// 
+//  History:
+//  03 Jul 2023  Arif Matin   Created, Harcourt Street, Dublin, Ireland.
+// 
+//  License:
+//  Proprietary NoFrixion.
+// -----------------------------------------------------------------------------
+
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace NoFrixion.MoneyMoov.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter)]
+    public class CustomRegexStringAttribute : DataTypeAttribute
+    {
+        public string RegexString { get; set; }
+
+        public CustomRegexStringAttribute(string regexString) : base(DataType.Text)
+        {
+            RegexString = regexString;
+        }
+
+        public override bool IsValid(object? value)
+        {
+            Regex matchRegex = new(RegexString);
+
+            if (value == null || string.IsNullOrWhiteSpace((string?)value))
+            {
+                return true;
+            }
+
+            return matchRegex.IsMatch((string)value);
+        }
+    }
+}
