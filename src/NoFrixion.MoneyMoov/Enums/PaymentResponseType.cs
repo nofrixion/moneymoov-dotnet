0b2d4695a2bc461cd6a17a3e4d089c8cbfa22b9a
Aaron Clauson <aaron@nofrixion.com>
2023-08-02 17:56:46 +0100

    Added new payment request related modesl to monyemoov.

---
+++ src/NoFrixion.MoneyMoov/Enums/PaymentResponseType.cs
@@ -0,0 +1,40 @@
+﻿// -----------------------------------------------------------------------------
+//  Filename: PaymentResponseType.cs
+// 
+//  Description: An enum to help determine the response type in case of 
+//  creating and processing payment request at the same time.
+// 
+//  Author(s):
+//  Saurav Maiti (saurav@nofrixion.com)
+// 
+//  History:
+//  29 11 2022  Saurav Maiti Created, Harcourt Street, Dublin, Ireland.
+// 
+//  License:
+//  MIT.
+// -----------------------------------------------------------------------------
+
+namespace NoFrixion.MoneyMoov;
+
+public enum PaymentResponseType
+{
+    /// <summary>
+    /// Default response type when it is not set explicitly.
+    /// </summary>
+    None,
+
+    /// <summary>
+    /// Response type when card payer authentication is setup.
+    /// </summary>
+    CardPayerAuthenticationSetupResponse,
+    
+    /// <summary>
+    /// Response type after submitting a card payment.
+    /// </summary>
+    CardPaymentResponse,
+
+    /// <summary>
+    /// Response type after submitting PISP payment.
+    /// </summary>
+    PaymentInitiationResponse
+}
---
+++ src/NoFrixion.MoneyMoov/Models/PaymentRequests/PaymentInitiationRequest.cs
@@ -0,0 +1,34 @@
+﻿//-----------------------------------------------------------------------------
+// Filename: PaymentInitiationRequest.cs
+// 
+// Description: Represents the fields that can be used to initiate a payment
+// initiation attempt.
+//
+// Author(s):
+// Aaron Clauson (aaron@nofrixion.com)
+// 
+// History:
+// 10 Nov 2022  Aaron Clauson   Created, Stillorgan Wood, Dublin, Ireland.
+// 
+// License:
+// MIT.
+//-----------------------------------------------------------------------------
+
+using System.ComponentModel.DataAnnotations;
+
+namespace NoFrixion.MoneyMoov.Models;
+
+#nullable disable
+
+public class PaymentInitiationRequest
+{
+    /// <summary>
+    /// This is the ID of the institution (bank) that the payer ha chosen.
+    /// </summary>
+    [Required]
+    public string ProviderID { get; set; } 
+
+    public decimal PartialAmount { get; set; }
+
+    public string RedirectToOriginUrl { get; set; }
+}
