893ea82c5a9a0406e9b2bc11d3b2318d23273e78
sauravmaiti22 <103389834+sauravmaiti22@users.noreply.github.com>
2023-10-18 18:22:40 +0100

    Added account metrics model

---
+++ src/NoFrixion.MoneyMoov/Models/Account/AccountMetrics.cs
@@ -0,0 +1,29 @@
+﻿// -----------------------------------------------------------------------------
+//  Filename: AccountMetrics.cs
+// 
+//  Description: Entity containing account metrics of a merchant..
+// 
+//  Author(s):
+//  Saurav Maiti (saurav@nofrixion.com)
+// 
+//  History:
+//  18 10 2023  Saurav Maiti Created, Harcourt Street, Dublin, Ireland.
+// 
+//  License:
+//  Proprietary NoFrixion.
+// -----------------------------------------------------------------------------
+
+namespace NoFrixion.MoneyMoov.Models;
+
+public class AccountMetrics
+{
+    public Guid MerchantID { get; set; }
+    
+    public CurrencyTypeEnum Currency { get; set; }
+    
+    public int NumberOfAccounts { get; set; }
+    
+    public decimal TotalBalance { get; set; }
+    
+    public decimal TotalAvailableBalance { get; set; }
+}
