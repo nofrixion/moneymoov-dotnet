11fcb1a19a482d701ec8cbc27f32372fef97bc50
sauravmaiti22 <103389834+sauravmaiti22@users.noreply.github.com>
2023-11-14 17:14:51 +0000

    Added BeneficiaryIdentifier and BeneficiaryUpdate models.

---
+++ src/NoFrixion.MoneyMoov/Models/Beneficiary/BeneficiaryIdentifier.cs
@@ -0,0 +1,40 @@
+﻿// -----------------------------------------------------------------------------
+//  Filename: BeneficiaryIdentifier.cs
+// 
+//  Description: BeneficiaryIdentifier model
+// 
+//  Author(s):
+//  Saurav Maiti (saurav@nofrixion.com)
+// 
+//  History:
+//  06 11 2023  Saurav Maiti Created, Harcourt Street, Dublin, Ireland.
+// 
+//  License:
+//  Proprietary NoFrixion.
+// -----------------------------------------------------------------------------
+
+#nullable disable
+namespace NoFrixion.MoneyMoov.Models;
+
+public class BeneficiaryIdentifier
+{
+    public Guid ID { get; set; }
+
+    public string AccountName { get; set; }
+    
+    public string AccountNumber { get; set; }
+
+    public string SortCode { get; set; }
+
+    public string BitcoinAddress { get; set; }
+
+    public Guid BeneficiaryID { get; set; }
+
+    public Guid MerchantID { get; set; }
+
+    public string IBAN { get; set; }
+
+    public DateTimeOffset Inserted { get; set; }
+
+    public bool IsEnabled { get; set; }
+}
