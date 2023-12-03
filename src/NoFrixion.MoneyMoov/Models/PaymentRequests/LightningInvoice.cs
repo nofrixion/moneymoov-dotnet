12c83e268affd71ed6135ca0963038febd9c2815
Aaron Clauson <aaron@nofrixion.com>
2023-12-03 19:58:45 +0000

    Mov LN invoice creation for pay element from worker to api.

---
+++ src/NoFrixion.MoneyMoov/Models/PaymentRequests/LightningInvoice.cs
@@ -0,0 +1,27 @@
+﻿//-----------------------------------------------------------------------------
+// Filename: LightningInvoice.cs
+//
+// Description: Represents a Bitcoin \Llighting invoice.
+//
+// Author(s):
+// Aaron Clauson (aaron@nofrixion.com)
+// 
+// History:
+// 01 May 2023  Aaron Clauson   Created, Stillorgan Wood, Dublin, Ireland.
+//
+// License: 
+// MIT.
+//-----------------------------------------------------------------------------
+
+namespace NoFrixion.MoneyMoov.Models;
+
+public class LightningInvoice
+{
+    public string Description { get; set; } = string.Empty;
+
+    public string PaymentRequest { get; set; } = string.Empty;
+
+    public string RHash { get; set; } = string.Empty;
+
+    public DateTimeOffset ExpiresAt { get; set; }
+}
