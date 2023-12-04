49090982ac9509192b66316d14de502765d4bb91
Aaron Clauson <aaron@nofrixion.com>
2023-12-04 14:25:24 +0000

    First cut at models for creating Payruns.

---
+++ src/NoFrixion.MoneyMoov/Models/Payruns/PayrunCreate.cs
@@ -0,0 +1,24 @@
+﻿//-----------------------------------------------------------------------------
+// Filename: PayrunCreate.cs
+// 
+// Description: Represent the model to create a new Payrun from a list of
+// invoices.
+// 
+// Author(s):
+// Aaron Clauson (aaron@nofrixion.com)
+// 
+// History:
+// 04 Dec 2023  Aaron Clauson   Created, Harcourt Street, Dublin, Ireland.
+// 
+// License:
+// MIT.
+//-----------------------------------------------------------------------------
+
+namespace NoFrixion.MoneyMoov.Models;
+
+public class PayrunCreate
+{
+   public string Name { get; set; } = string.Empty;
+
+    public List<PayrunInvoice> Invoices { get; set; } = new List<PayrunInvoice>();
+}
