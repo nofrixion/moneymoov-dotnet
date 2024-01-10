7b31b7915a69e86ae757a53f8b15c2ddce486fa4
sauravmaiti22 <103389834+sauravmaiti22@users.noreply.github.com>
2024-01-10 12:08:27 +0000

    Added check in SynchroniseAccount

---
+++ src/NoFrixion.MoneyMoov/Constants/OpenBankingConstants.cs
@@ -0,0 +1,25 @@
+﻿// -----------------------------------------------------------------------------
+//  Filename: OpenBankingConstants.cs
+// 
+//  Description: Constants related to open banking.
+// 
+//  Author(s):
+//  Saurav Maiti (saurav@nofrixion.com)
+// 
+//  History:
+//  10 1 2024  Saurav Maiti Created, Harcourt Street, Dublin, Ireland.
+// 
+//  License:
+//  Proprietary NoFrixion.
+// -----------------------------------------------------------------------------
+
+namespace NoFrixion.MoneyMoov.Constants;
+
+public class OpenBankingConstants
+{
+    public const string YAPILY_CONSENT_AUTHORIZED_STATUS = "AUTHORIZED";
+    
+    public const string YAPILY_CONSENT_AWAITING_AUTHORIZATION_STATUS = "AWAITING_AUTHORIZATION";
+    
+    public const string YAPILY_CONSENT_AWAITING_REAUTHORIZATION_STATUS = "AWAITING_RE_AUTHORIZATION";
+}
