8c37b7247ad99b6a7bd3acc61beb898e6ffce5d6
Aaron Clauson <aaron@nofrixion.com>
2024-01-17 09:24:27 +0000

    Added report result.

---
+++ src/NoFrixion.MoneyMoov/Models/Reports/ReportResult.cs
@@ -0,0 +1,27 @@
+//-----------------------------------------------------------------------------
+// Filename: ReportResult.cs
+// 
+// Description: Model to represent the result of a NoFrixion report execution.
+// Reports are things such  as an account transaction statement or SWIFT report.
+// 
+// Author(s):
+// Aaron Clauson (aaron@nofrixion.com)
+// 
+// History:
+// 15 Jan 2024  Aaron Clauson   Created, Harcourt Street, Dublin, Ireland.
+// 
+// License:
+// MIT.
+//-----------------------------------------------------------------------------
+
+namespace NoFrixion.MoneyMoov.Models;
+
+public class ReportResult
+{
+    public Guid MerchantID { get; set; }
+    public ReportTypesEnum ReportType { get; set; }
+    public string ReportName { get; set; } = string.Empty;
+    public DateTimeOffset LastCompletedAt { get; set; }
+    public int StatementNumber { get; set; }
+    public string Contents { get; set; } = string.Empty;
+}
