d7291c7791ba918a24ffc4c9342dcf3cb5a64ec2
Aaron Clauson <aaron@nofrixion.com>
2024-05-20 16:10:41 +0100

    wip: Replacing Rule SweepAction with RuleDestinations.

--- src/NoFrixion.MoneyMoov/Models/Rules/RuleActions/SweepDestination.cs
+++ src/NoFrixion.MoneyMoov/Models/Rules/RuleActions/SweepDestination.cs
@@ -40,6 +40,13 @@ public class SweepDestination : Counterparty, IValidatableObject
     [Range(0, 1000)]
     public int Priority { get; set; }
 
+    /// <summary>
+    /// Optional ID of a Beneficairy to use for the sweep destination. If the Beneficiary ID is set then the 
+    /// Identifier details will be ignored and instead will be set to the details of the Beneficiary at the point
+    /// the rule is executed.
+    /// </summary>
+    public Guid? BeneficiaryID { get; set; }
+
     public override Dictionary<string, string> ToDictionary(string keyPrefix)
     {
         var dict = base.ToDictionary(keyPrefix);
