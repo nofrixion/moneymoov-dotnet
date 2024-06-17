00e10a5359cff20fb3f7ecedd1a7b1bd5493f838
Aaron Clauson <aaron@nofrixion.com>
2024-06-17 09:37:44 +0100

    Adds a dedicated method for determining payment request expiry (#358)
    * Improve payment request expiry checking.
    
    * Added pay by bank expiry method for payment attempts.
    
    * Added new model for sandbox pay by bank simulations.

---
+++ src/NoFrixion.MoneyMoov/Models/PaymentRequests/PaymentRequestPispSandboxCallback.cs
@@ -0,0 +1,29 @@
+﻿//-----------------------------------------------------------------------------
+// Filename: PaymentRequestPispSandboxCallback.cs
+// 
+// Description: Payment initiation callback model for pay by bank simulations.
+// 
+// Author(s):
+// Aaron Clauson (aaron@nofrixion.com)
+// 
+// History:
+// 16 Jun 2024  Aaron Clauson   Created, Stillorgan Wood, Dublin, Ireland.
+// 
+// License:
+// Proprietary NoFrixion.
+//-----------------------------------------------------------------------------
+
+namespace NoFrixion.MoneyMoov.Models;
+
+public class PaymentRequestPispSandboxCallback
+{
+   public decimal Amount { get; set; }
+
+   public string? Institution { get; set; }
+
+   public string? PaymentInitiationID { get; set; }
+            
+   public string? ErrorDescription { get; set; }
+            
+   public bool DoSimulateSettlementFailure { get; set; }
+}
