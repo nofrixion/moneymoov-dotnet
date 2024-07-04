0156a756f67a17f781546ff543073d6892317d04
sauravmaiti22 <saurav@nofrixion.com>
2024-07-04 17:20:19 +0100

    Added tenant name to Xero connection model

--- src/NoFrixion.MoneyMoov/Models/Xero/XeroConnection.cs
+++ src/NoFrixion.MoneyMoov/Models/Xero/XeroConnection.cs
@@ -6,4 +6,6 @@ public class XeroConnection
     public Guid MerchantID { get; set; }
     
     public DateTimeOffset ConnectedAt { get; set; }
+    
+    public string XeroTenantName { get; set; }
 }
