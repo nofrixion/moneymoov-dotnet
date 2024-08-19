a9bcfcafdfc9575cb90225a022a3449b6720a77f
sauravmaiti22 <saurav@nofrixion.com>
2024-08-19 19:27:11 +0100

    Added summary

--- src/NoFrixion.MoneyMoov/Models/Payouts/PayoutsDeleteResponse.cs
+++ src/NoFrixion.MoneyMoov/Models/Payouts/PayoutsDeleteResponse.cs
@@ -1,3 +1,18 @@
+// -----------------------------------------------------------------------------
+//  Filename: PayoutsDeleteResponse.cs
+// 
+//  Description: Contains the response for deleting multiple payouts in a single request.
+//
+//  Author(s):
+//  Saurav Maiti (saurav@nofrixion.com)
+// 
+//  History:
+//  19 Aug 2024  Saurav Maiti   Created, Harcourt Street, Dublin, Ireland.
+// 
+//  License:
+//  MIT
+// -----------------------------------------------------------------------------
+
 namespace NoFrixion.MoneyMoov.Models;
 
 public class PayoutsDeleteResponse
