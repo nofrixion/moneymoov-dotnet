b89ef98068f0e2ea010e0db1583bb9724898faa5
sauravmaiti22 <saurav@nofrixion.com>
2024-08-25 14:27:55 +0100

    Allow deletion of non-approved payruns.

---
+++ src/NoFrixion.MoneyMoov/Extensions/PayrunStatusExtensions.cs
@@ -0,0 +1,27 @@
+// -----------------------------------------------------------------------------
+//  Filename: PayrunStatusExtensions.cs
+// 
+//  Description: Extensions for PayrunStatus enum
+// 
+//  Author(s):
+//  Saurav Maiti (saurav@nofrixion.com)
+// 
+//  History:
+//  25 08 2024  Saurav Maiti   Created, Harcourt St, Dublin, Ireland.
+// 
+//  License:
+//  Proprietary NoFrixion.
+// -----------------------------------------------------------------------------
+
+using NoFrixion.MoneyMoov.Enums;
+
+namespace NoFrixion.MoneyMoov.Extensions;
+
+public static class PayrunStatusExtensions
+{
+    public static bool CanEdit(this PayrunStatus status) =>
+        status is PayrunStatus.Draft or PayrunStatus.AuthorisationPending or PayrunStatus.Edited or PayrunStatus.Rejected;
+    
+    public static bool CanDelete(this PayrunStatus status) =>
+        status is PayrunStatus.Draft or PayrunStatus.Edited or PayrunStatus.Rejected or PayrunStatus.AuthorisationPending;
+}
