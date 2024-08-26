c1d6ef5af3432ac0147864fe73a04a4fa22812f8
sauravmaiti22 <saurav@nofrixion.com>
2024-08-26 12:06:02 +0100

    Added summary to pay run states.

--- src/NoFrixion.MoneyMoov/Enums/PayrunStatus.cs
+++ src/NoFrixion.MoneyMoov/Enums/PayrunStatus.cs
@@ -19,12 +19,46 @@ namespace NoFrixion.MoneyMoov.Enums;
 public enum PayrunStatus
 {
     None = 0,
+    
+    /// <summary>
+    /// Indicates that the pay run is in draft mode.
+    /// </summary>
     Draft = 1,
-    Queued = 2,
-    Submitted = 3,
-    Completed = 5, 
-    Rejected = 6,
-    AuthorisationPending = 7,
-    PayoutsCreated = 8,
-    Edited = 9,
+    
+    /// <summary>
+    /// Indicates that all the payouts in the pay run
+    /// have been submitted for processing.
+    /// </summary>
+    Submitted = 2,
+    
+    /// <summary>
+    /// Indicates that the pay run has been completed.
+    /// </summary>
+    Completed = 3, 
+    
+    /// <summary>
+    /// Indicates that the pay run was rejected by the approver.
+    /// </summary>
+    Rejected = 4,
+    
+    /// <summary>
+    /// Indicates that approval was requested for the pay run
+    /// and is waiting for approval.
+    /// </summary>
+    AuthorisationPending = 5,
+    
+    /// <summary>
+    /// Indicates that the pay run was approved and payouts were created.
+    /// </summary>
+    PayoutsCreated = 6,
+    
+    /// <summary>
+    /// Indicates that the pay run was edited.
+    /// </summary>
+    Edited = 7,
+    
+    /// <summary>
+    /// Indicates that the pay run has been authorised and is queued for processing.
+    /// </summary>
+    Queued = 8,
 }
