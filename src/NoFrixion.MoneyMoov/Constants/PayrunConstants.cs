76069187e1ae3116d3e39c4d2ef9d3edc4cbada3
sauravmaiti22 <saurav@nofrixion.com>
2024-10-23 14:47:37 +0100

    Accept multiple emails for pay run remittance.

---
+++ src/NoFrixion.MoneyMoov/Constants/PayrunConstants.cs
@@ -0,0 +1,7 @@
+namespace NoFrixion.MoneyMoov.Constants;
+
+public static class PayrunConstants
+{
+    public const string REMMITANCE_EMAIL_ADDRESSES_ERROR_MESSAGE =
+        "One or more of the remittance email addresses are invalid. Addresses can be separated by a comma, semi-colon or space.";
+}
