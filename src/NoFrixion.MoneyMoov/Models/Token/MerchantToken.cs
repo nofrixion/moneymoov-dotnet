fee3a513d64251bdd2787c48baa1e8eabf243182
Aaron Clauson <aaron@nofrixion.com>
2024-11-22 21:44:59 +0000

    Added extra properties to merfhant token for shared secret operation.

--- src/NoFrixion.MoneyMoov/Models/Token/MerchantToken.cs
+++ src/NoFrixion.MoneyMoov/Models/Token/MerchantToken.cs
@@ -37,4 +37,33 @@ public class MerchantToken
     /// initially created. The token is not stored by NoFrixion.
     /// </summary>
     public string Token { get; set; }
+
+    /// <summary>
+    /// If set to false the merchant token will not be accepted to authorise a request.
+    /// </summary>
+    public bool IsEnabled { get; set; }
+
+    /// <summary>
+    /// Optional shared secret algorithm to use for HMAC authentication.
+    /// </summary>
+    public SharedSecretAlgorithmsEnum SharedSecretAlgorithm { get; set; }
+
+    /// <summary>
+    /// The base 64 encoded shared secret that is used for request authentication with an HMAC.
+    /// Not this property will ONLY be set when the token is initially created. It is not possible
+    /// to retrieve the secret afterwards. If it is lost a new token should be created.
+    /// </summary>
+    public string SharedSecretBase64 { get; set; }
+
+    /// <summary>
+    /// Represent the version of the overall merchant token. This field is to allow the secret and public key mechanisms to
+    /// vary over time. For example if the HTTP header fields to include in the algorithms change this version will faciliatate
+    /// keeping track of which signature versions a particular merchant token is using.
+    /// </summary>
+    public int RequestSignatureVersion { get; set; }
+
+    /// <summary>
+    /// Optional. If set indicates the merchant token is not valid after the specified expiry date.
+    /// </summary>
+    public DateTimeOffset? ExpiresAt { get; set; }
 }
