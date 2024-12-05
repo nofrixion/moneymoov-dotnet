5c3fdd3cac20cb9bb8c7856281f7a8f99537d256
donalnofrixion <89344328+donalnofrixion@users.noreply.github.com>
2024-12-05 12:52:36 +0000

    Converted permissions flag enums to lists for public models

--- src/NoFrixion.MoneyMoov/Models/Roles/RoleCreate.cs
+++ src/NoFrixion.MoneyMoov/Models/Roles/RoleCreate.cs
@@ -18,7 +18,7 @@ using NoFrixion.Common.Permissions;
 
 namespace NoFrixion.MoneyMoov.Models.Roles;
 
-public class RoleCreate
+public class RoleCreate : IValidatableObject
 {
     [Required]
     public string Name { get; set; } = null!;
@@ -30,10 +30,27 @@ public class RoleCreate
     public Guid MerchantID { get; set; }
     
     [Required]
-    public MerchantPermissions MerchantPermissions { get; set; }
+    public List<string> MerchantPermissions { get; set; } = new();
 
     [Required]
-    public AccountPermissions AccountPermissions { get; set; }
+    public List<string> AccountPermissions { get; set; } = new();
     
     public List<RoleUserCreate>? Users { get; set; }
+    
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (AccountPermissions.Any(p => !Enum.TryParse<AccountPermissions>(p, out _)))
+        {
+            var validAccountPermissions = string.Join(", ", Enum.GetValues(typeof(AccountPermissions)).Cast<AccountPermissions>());
+            
+            yield return new ValidationResult($"Invalid account permission. Valid permissions are {validAccountPermissions}", [nameof(AccountPermissions)]);
+        }
+        
+        if (MerchantPermissions.Any(p => !Enum.TryParse<MerchantPermissions>(p, out _)))
+        {
+            var validMerchantPermissions = string.Join(", ", Enum.GetValues(typeof(MerchantPermissions)).Cast<MerchantPermissions>());
+            
+            yield return new ValidationResult($"Invalid merchant permission. Valid permissions are {validMerchantPermissions}", [nameof(MerchantPermissions)]);
+        }
+    }
 }
--- src/NoFrixion.MoneyMoov/Models/Roles/RoleUpdate.cs
+++ src/NoFrixion.MoneyMoov/Models/Roles/RoleUpdate.cs
@@ -13,11 +13,12 @@
 //  Proprietary NoFrixion.
 // -----------------------------------------------------------------------------
 
+using System.ComponentModel.DataAnnotations;
 using NoFrixion.Common.Permissions;
 
 namespace NoFrixion.MoneyMoov.Models.Roles;
 
-public class RoleUpdate
+public class RoleUpdate : IValidatableObject
 {
     /// <summary>
     /// The role id
@@ -37,10 +38,27 @@ public class RoleUpdate
     /// <summary>
     /// The roles merchant permissions
     /// </summary>
-    public MerchantPermissions? MerchantPermissions { get; set; }
+    public List<string>? MerchantPermissions { get; set; }
 
     /// <summary>
     /// The roles account permissions
     /// </summary>
-    public AccountPermissions? AccountPermissions { get; set; }
+    public List<string>? AccountPermissions { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (AccountPermissions != null && AccountPermissions.Any(p => !Enum.TryParse<AccountPermissions>(p, out _)))
+        {
+            var validAccountPermissions = string.Join(", ", Enum.GetValues(typeof(AccountPermissions)).Cast<AccountPermissions>());
+            
+            yield return new ValidationResult($"Invalid account permission. Valid permissions are {validAccountPermissions}", [nameof(AccountPermissions)]);
+        }
+        
+        if (MerchantPermissions != null && MerchantPermissions.Any(p => !Enum.TryParse<MerchantPermissions>(p, out _)))
+        {
+            var validMerchantPermissions = string.Join(", ", Enum.GetValues(typeof(MerchantPermissions)).Cast<MerchantPermissions>());
+            
+            yield return new ValidationResult($"Invalid merchant permission. Valid permissions are {validMerchantPermissions}", [nameof(MerchantPermissions)]);
+        }
+    }
 }
