8db2c5502c382c12cc09f349e3bd03b89ef3c8ad
sauravmaiti22 <saurav@nofrixion.com>
2025-01-16 12:58:21 +0000

    Roles assigned new template changes

---
+++ src/NoFrixion.MoneyMoov/Constants/RoleConstants.cs
@@ -0,0 +1,21 @@
+//-----------------------------------------------------------------------------
+// Filename: RoleConstants.cs
+// 
+// Description: Contains all the constants related to user roles and permissions
+// 
+// Author(s):
+// Saurav Maiti (saurav@nofrixion.com)
+// 
+// History:
+// 16 Jan 2025  Saurav Maiti   Created, Hamilton gardens, Dublin, Ireland. 
+// 
+// License:
+// Proprietary NoFrixion.
+//-----------------------------------------------------------------------------
+
+namespace NoFrixion.MoneyMoov.Constants;
+
+public static class RoleConstants
+{
+    public const string ALL_ACCOUNTS_ROLE_SCOPE_TEXT = "All Accounts";
+}
