dba8abb4334e3aab4372a7976af13ca7eb4629e5
Pablo Maldonado <pablomartinmaldonado1@gmail.com>
2025-03-19 09:49:57 -0300

    feat: add suspension reason when suspending merchants

---
+++ src/NoFrixion.MoneyMoov/Models/Merchant/MerchantSuspend.cs
@@ -0,0 +1,25 @@
+﻿// -----------------------------------------------------------------------------
+//  Filename: MerchantSuspend.cs
+// 
+//  Description: Class containing information for a merchant to be suspended:
+// 
+//  Author(s):
+//  Pablo Maldonado (pablo@nofrixion.com)
+// 
+//  History:
+//  18 03 2025  Pablo Maldonado   Created, Montevideo, Uruguay.
+// 
+//  License:
+//  MIT.
+// -----------------------------------------------------------------------------
+
+namespace NoFrixion.MoneyMoov.Models;
+
+public class MerchantSuspend
+{
+    /// <summary>
+    /// The reason for the suspension.
+    /// </summary>
+    public string? Reason { get; set; }
+
+}
