b9eb96e01dd348c1fec69d52bd764d837fdc6f23
donalnofrixion <89344328+donalnofrixion@users.noreply.github.com>
2025-04-07 11:02:42 +0100

    Payment request template

---
+++ src/NoFrixion.MoneyMoov/Models/PaymentRequests/MerchantPaymentRequestTemplate.cs
@@ -0,0 +1,33 @@
+// -----------------------------------------------------------------------------
+//  Filename: MerchantPaymentRequestTemplate.cs
+// 
+//  Description: Merchant payment request template.:
+// 
+//  Author(s):
+//  Donal O'Connor (donal@nofrixion.com)
+// 
+//  History:
+//  04 04 2025  Donal O'Connor   Created, Harcourt St, Dublin, Ireland.
+// 
+//  License:
+//  Proprietary NoFrixion.
+// -----------------------------------------------------------------------------
+
+namespace NoFrixion.MoneyMoov.Models.PaymentRequests;
+
+public class MerchantPaymentRequestTemplate
+{
+    public Guid ID { get; set; }
+    
+    public Guid MerchantID { get; set; }
+    
+    public required string Name { get; set; }
+    
+    public required string Description { get; set; }
+
+    public required PaymentRequestTemplate Template { get; set; }
+    
+    public DateTimeOffset Inserted { get; set; }
+
+    public DateTimeOffset LastUpdated { get; set; }
+}
---
+++ src/NoFrixion.MoneyMoov/Models/PaymentRequests/PaymentRequestTemplateUpdate.cs
@@ -0,0 +1,29 @@
+// -----------------------------------------------------------------------------
+//  Filename: PaymentRequestTemplateUpdate.cs
+// 
+//  Description: Contains details of a PaymentRequestTemplateUpdate model:
+// 
+//  Author(s):
+//  Donal O'Connor (donal@nofrixion.com)
+// 
+//  History:
+//  07 04 2025  Donal O'Connor   Created, Harcourt St, Dublin, Ireland.
+// 
+//  License:
+//  Proprietary NoFrixion.
+// -----------------------------------------------------------------------------
+
+namespace NoFrixion.MoneyMoov.Models.PaymentRequests;
+
+public class PaymentRequestTemplateUpdate
+{
+    public Guid ID { get; set; }
+    
+    public Guid MerchantID { get; set; }
+    
+    public required string Name { get; set; }
+    
+    public required string Description { get; set; }
+    
+    public required PaymentRequestTemplate Template { get; set; }
+}
