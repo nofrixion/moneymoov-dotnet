ab63ffff8d3caf4d50ff8dd2cd332377cf0f4187
arifm <arifm@ARIFMATIN>
2025-04-09 17:41:38 +0100

    WIP - Add multiple role endpoints

---
+++ src/NoFrixion.MoneyMoov/Models/Roles/RolesUserCreate.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoFrixion.MoneyMoov.Models.Roles
+{
+    public class RolesUserCreate
+    {
+        public  List<RoleUserCreate>? RolesUser { get; set; }
+    }
+}
