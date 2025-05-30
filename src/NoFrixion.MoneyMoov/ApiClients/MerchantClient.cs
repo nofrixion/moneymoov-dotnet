0f1041030a002684629365e6d63f68a74abe901b
arifm <arifm@ARIFMATIN>
2025-05-30 17:41:17 +0100

    WIP – Edit roles

--- src/NoFrixion.MoneyMoov/ApiClients/MerchantClient.cs
+++ src/NoFrixion.MoneyMoov/ApiClients/MerchantClient.cs
@@ -40,7 +40,9 @@ public interface IMerchantClient
     Task<RestApiResponse<IEnumerable<UserInvite>>> GetUserInvitesAsync(string userAccessToken, Guid merchantID);
     
     Task<RestApiResponse<IEnumerable<Role>>> GetRolesAsync(string userAccessToken, Guid merchantID);
-    
+
+    Task<RestApiResponse<Role>> GetRoleAsync(string userAccessToken, Guid merchantID, Guid roleID);
+
     Task<RestApiResponse<Role>> AddUserToRole(string userAccessToken, RoleUserCreate roleUserCreate, Guid merchantID, Guid roleID);
     
     Task<RestApiResponse> DeleteUserFromRole(string userAccessToken, Guid merchantID, Guid roleID, Guid userID);
@@ -215,7 +217,27 @@ public class MerchantClient : IMerchantClient
             _ => Task.FromResult(new RestApiResponse<IEnumerable<Role>>(HttpStatusCode.PreconditionFailed, new Uri(url), prob))
         };
     }
-    
+
+    /// <summary>
+    /// Gets a specific merchant role by its ID.
+    /// </summary>
+    /// <param name="userAccessToken">A user scoped JWT access token.</param>
+    /// <param name="merchantID">The ID of the merchant.</param>
+    /// <param name="roleID">The role ID to get.</param>
+    /// <returns>If successful, the merchant role.</returns>
+    public Task<RestApiResponse<Role>> GetRoleAsync(string userAccessToken, Guid merchantID, Guid roleID)
+    {
+        var url = MoneyMoovUrlBuilder.MerchantsApi.MerchantRoleUrl(_apiClient.GetBaseUri().ToString(), merchantID, roleID);
+
+        var prob = _apiClient.CheckAccessToken(userAccessToken, nameof(GetRolesAsync));
+
+        return prob switch
+        {
+            var p when p.IsEmpty => _apiClient.GetAsync<Role>(url, userAccessToken),
+            _ => Task.FromResult(new RestApiResponse<Role>(HttpStatusCode.PreconditionFailed, new Uri(url), prob))
+        };
+    }
+
     public Task<RestApiResponse<Role>> AddUserToRole(string userAccessToken, RoleUserCreate roleUserCreate, Guid merchantID, Guid roleID)
     {
         var url = MoneyMoovUrlBuilder.MerchantsApi.MerchantRoleUsersUrl(_apiClient.GetBaseUri().ToString(), merchantID, roleID);
