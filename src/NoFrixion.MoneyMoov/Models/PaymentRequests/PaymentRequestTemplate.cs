e6f10e2e321695691b26431e85ffe23af6dd412b
sauravmaiti22 <saurav@nofrixion.com>
2025-06-03 13:54:29 +0100

    Updated default template

--- src/NoFrixion.MoneyMoov/Models/PaymentRequests/PaymentRequestTemplate.cs
+++ src/NoFrixion.MoneyMoov/Models/PaymentRequests/PaymentRequestTemplate.cs
@@ -167,6 +167,10 @@ public class DefaultPaymentRequestTemplate
         {
             DestinationAccounts = null,
         },
+        PriorityBankOptions = new PriorityBankOptions()
+        {
+            PriorityBank = null
+        },
         CardPaymentAddressOptions = new CardPaymentAddressOptions
         {
             RequireAddress = false
@@ -199,6 +203,7 @@ public class DefaultPaymentRequestTemplate
                 DisplayForPayer = true,
                 DefaultField = PaymentRequestDefaultFieldsEnum.DueDate
             }
-        ]
+        ],
+        CustomFields = []
     };
 }
