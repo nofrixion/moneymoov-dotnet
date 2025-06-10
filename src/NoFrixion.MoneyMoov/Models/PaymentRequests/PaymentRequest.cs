30f016211975d66ef60eb035ebd9e38e04f8f1e2
sauravmaiti22 <saurav@nofrixion.com>
2025-06-10 11:25:26 +0100

    Added notification role IDs to payment request.

--- src/NoFrixion.MoneyMoov/Models/PaymentRequests/PaymentRequest.cs
+++ src/NoFrixion.MoneyMoov/Models/PaymentRequests/PaymentRequest.cs
@@ -382,6 +382,11 @@ public class PaymentRequest : IPaymentRequest, IWebhookPayload, IExportableToCsv
     /// A list of field display settings that control which fields are displayed to the payer.
     /// </summary>
     public List<PaymentRequestFieldDisplaySetting>? FieldDisplaySettings { get; set; }
+    
+    /// <summary>
+    /// A list of roles whose members will receive notifications about this payment request.
+    /// </summary>
+    public List<Guid>? NotificationRoleIDs { get; set; }
 
     public string CustomerName =>
         Addresses.Any() ? $"{Addresses.First().FirstName} {Addresses.First().LastName}" : string.Empty;
