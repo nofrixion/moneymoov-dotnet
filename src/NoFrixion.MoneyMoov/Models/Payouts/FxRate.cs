552e39cbe2b9a66c7e9b72807f2f6cabdf324d2b
Aaron Clauson <aaron@nofrixion.com>
2025-07-18 14:08:40 +0100

    Added FxRate model for multi-currency payouts.

---
+++ src/NoFrixion.MoneyMoov/Models/Payouts/FxRate.cs
@@ -0,0 +1,36 @@
+﻿//-----------------------------------------------------------------------------
+// Filename: FxRate.cs
+// 
+// Description: A model representing a Foreign Exhange (FX) rate.
+// 
+// Author(s):
+// Aaron Clasuon (aaron@nofrixion.com)
+// 
+// History:
+// 17 Jul 2025  Aaron Clauson   Created, Stillorgan Wood, Dublin, Ireland.
+// 
+// License:
+// Proprietary NoFrixion.
+//-----------------------------------------------------------------------------
+
+namespace NoFrixion.MoneyMoov.Models;
+
+/// <summary>
+/// Represents a Foreign Exhange (FX) rate.
+/// </summary>
+public class FxRate
+{
+    public CurrencyTypeEnum SourceCurrency { get; set; }
+
+    public CurrencyTypeEnum DestinationCurrency { get; set; }
+
+    /// <summary>
+    /// The price at which the transaction will buy the source currency 
+    /// using the destination currency.
+    /// </summary>
+    public decimal ExchangeRate { get; set; }
+
+    public string? QuoteID { get; set; }
+
+    public DateTimeOffset ExpiryTime { get; set; }
+}
