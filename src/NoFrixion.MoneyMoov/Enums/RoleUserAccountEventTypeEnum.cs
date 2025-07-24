ce45a7526d049c2dbde3623cd87d593af9ef4b3f
Axel Granillo <axel@nofrixion.com>
2025-07-24 01:08:52 -0600

    MOOV-4708: Add enums for RoleUserEvents and RoleUserAccountEvents

---
+++ src/NoFrixion.MoneyMoov/Enums/RoleUserAccountEventTypeEnum.cs
@@ -0,0 +1,23 @@
+﻿// -----------------------------------------------------------------------------
+//  Filename: RoleUserAccountEventTypeEnum.cs
+// 
+//  Description: List of the types of events that can occur in a role-user-account assignment:
+// 
+//  Author(s):
+//  Axel Granillo (axel@nofrixion.com)
+// 
+//  History:
+//  22 07 2025  Axel Granillo   Created, Remote, Mexico City, Mexico.
+// 
+//  License:
+//  Proprietary NoFrixion.
+// -----------------------------------------------------------------------------
+
+namespace NoFrixion.MoneyMoov.Enums;
+
+public enum RoleUserAccountEventTypeEnum
+{
+    None = 0,
+    
+    AssignedToRoleUser = 1,
+}
