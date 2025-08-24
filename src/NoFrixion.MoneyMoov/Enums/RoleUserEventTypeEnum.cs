e1b55b11c8462655a8a24fdb5ea09a27b04d42e6
Aaron Clauson <aaron@nofrixion.com>
2025-08-24 16:05:19 +0100

    Adds the RoleUser authorisation type (#636)
    * Adding role user authorisation for initial assignment.
    
    * Adjusted the role user hash.
    
    * Added role user event types enum.

---
+++ src/NoFrixion.MoneyMoov/Enums/RoleUserEventTypeEnum.cs
@@ -0,0 +1,30 @@
+﻿//-----------------------------------------------------------------------------
+// Filename: RoleUserEventTypeEnum.cs
+// 
+// Description: Enum for the different types of role user (assigning a user to a role)
+// events that can occur.
+// 
+// Author(s):
+// Aaron Clauson (aaron@nofrixion.com)
+// 
+// History:
+// 22 Aug 2025  Aaron Clauson   Created, Stillorgan Wood, Dublin, Ireland.
+// 
+// License:
+// MIT.
+//-----------------------------------------------------------------------------
+
+namespace NoFrixion.MoneyMoov.Enums;
+
+public enum RoleUserEventTypeEnum
+{
+    /// <summary>
+    /// Something went wrong and the event type is unknown.
+    /// </summary>
+    Unknown = 0,
+    
+    /// <summary>
+    /// A user invite was authorised.
+    /// </summary>
+    Authorise = 1
+}
