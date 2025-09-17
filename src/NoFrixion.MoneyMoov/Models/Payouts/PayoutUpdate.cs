2b419c60de6df1bebc0afff962a69e739d452078
Aaron Clauson <aaron@nofrixion.com>
2025-09-17 14:48:50 +0100

    Spelling mistakes.

--- src/NoFrixion.MoneyMoov/Models/Payouts/PayoutUpdate.cs
+++ src/NoFrixion.MoneyMoov/Models/Payouts/PayoutUpdate.cs
@@ -37,7 +37,7 @@ public class PayoutUpdate
     /// <summary>
     /// The Your reference field is an optional field that gets set locally on the payer's transaction record. It does not get 
     /// sent out through the payment network. The maximum length for the field is 256 characters. An empty value is also supported.
-    /// Supported chracters:
+    /// Supported characters:
     /// a b c d e f g h i j k l m n o p q r s t u v w x y z
     /// A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
     /// 0 1 2 3 4 5 6 7 8 9
