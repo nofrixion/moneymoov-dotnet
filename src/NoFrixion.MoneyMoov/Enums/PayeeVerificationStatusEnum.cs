be717b676a82ed2cf16393321689594c5fbc094e
sauravmaiti22 <saurav@nofrixion.com>
2025-10-01 11:06:39 +0100

    Add none to enum

--- src/NoFrixion.MoneyMoov/Enums/PayeeVerificationStatusEnum.cs
+++ src/NoFrixion.MoneyMoov/Enums/PayeeVerificationStatusEnum.cs
@@ -3,22 +3,27 @@ namespace NoFrixion.MoneyMoov.Enums;
 public enum PayeeVerificationStatusEnum
 {
     /// <summary>
+    /// Indicates that no payee verification status is set.
+    /// </summary>
+    None = 0,
+    
+    /// <summary>
     /// Indicates that payee verification is not required for this payee.
     /// </summary>
-    NotRequired = 0,
+    NotRequired = 1,
     
     /// <summary>
     /// Indicates that payee verification is pending and has not yet been completed.
     /// </summary>
-    Pending = 1,
+    Pending = 2,
     
     /// <summary>
     /// Indicates that payee verification has been successfully completed.
     /// </summary>
-    Completed = 2,
+    Completed = 3,
     
     /// <summary>
     /// Indicates that payee verification failed.
     /// </summary>
-    Failed = 3
+    Failed = 4
 }
