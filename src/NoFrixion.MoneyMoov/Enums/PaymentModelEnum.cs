623725d629b15c473349dd75e06149694f3cc372
Axel Granillo <axel@nofrixion.com>
2025-12-22 19:15:10 -0600

    feat: Add PaymentModel enum, primarily for Direct Debit

---
+++ src/NoFrixion.MoneyMoov/Enums/PaymentModelEnum.cs
@@ -0,0 +1,31 @@
+﻿// -----------------------------------------------------------------------------
+//  Filename: PaymentModelEnum.cs
+// 
+//  Description: 
+// 
+//  Author(s):
+//  Axel Granillo (axel@nofrixion.com)
+// 
+//  History:
+//  22 12 2025  Axel Granillo   Created, Remote, Mexico City, Mexico.
+// 
+//  License:
+//  Proprietary NoFrixion.
+// -----------------------------------------------------------------------------
+
+namespace NoFrixion.MoneyMoov.Enums;
+
+public enum PaymentModelEnum
+{
+    None,
+    
+    /// <summary>
+    /// Entity executes the payment on behalf of the merchant.
+    /// </summary>
+    PoBo,
+    
+    /// <summary>
+    /// Platform acts as an agent, while the underlying merchant or company remains the legal creditor.
+    /// </summary>
+    Agency
+}
